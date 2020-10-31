be44ca0094db384c668774523cafb14e1952dec9
Nzubechukwu <nzubennamani@gmail.com>
2020-10-31 17:22:45 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import TouchScreen from './src/screens/TouchScreen'
+import PinScreen from './src/screens/PinScreen'
+import HomeScreen from './src/screens/HomeScreen'
+import SendRequestScreen from './src/screens/SendRequestScreen'
+import CardsScreen from './src/screens/CardsScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const AppStack = createStackNavigator()
+  const TabStack = createBottomTabNavigator()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const tabBarOptions = {
+    showLabel: true,
+    style: {
+      backgroundColor: '#1e1e1e',
+      borderTopColor: '#1e1e1e',
+      paddingBottom: 32,
+    },
+  }
+
+  const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused }) => {
+      let icon = ''
+      const color = focused ? '#559dff' : '#828282'
+      const size = 24
+
+      switch (route.name) {
+        case 'Cards':
+          icon = 'credit-card'
+          break
+
+        case 'SendRequest':
+          icon = 'send'
+          break
+
+        default:
+          icon = 'dashboard'
+      }
+      return <MaterialIcons name={icon} size={size} color={color} />
+    },
+  })
+
+  const TabStackScreens = () => {
+    return (
+      <TabStack.Navigator
+        tabBarOptions={tabBarOptions}
+        screenOptions={screenOptions}
+      >
+        <TabStack.Screen name="Home" component={HomeScreen} />
+        <TabStack.Screen
+          name="Cards"
+          component={CardsScreen}
+          options={{ title: 'My Cards' }}
+        />
+        <TabStack.Screen
+          name="SendRequest"
+          component={SendRequestScreen}
+          options={{ title: 'Send & Request' }}
+        />
+      </TabStack.Navigator>
+    )
+  }
+
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator headerMode="none">
+        <AppStack.Screen name="Touch" component={TouchScreen} />
+        <AppStack.Screen name="Pin" component={PinScreen} />
+        <AppStack.Screen name="Tabs" component={TabStackScreens} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  )
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# mybank app
+
+mybank app is a react native banking application. It is an academic project designed to showcase some advanced react native skills.
+
+## Installation
+
+- Ensure you have expo installed already or click  [here](https://docs.expo.io/) to install expo.
+
+```bash
+npm install --global expo-cli
+```
+- clone this [repo](https://github.com/Alpha1202/mybank-react-native-tutorial.git)
+
+
+- cd into the app and run 
+```bash
+npm install
+```
+
+- finally, run 
+```bash
+npm run start
+```
+## Overview
+
+![alt text](https://res.cloudinary.com/de8vrxbqq/image/upload/v1604160773/samples/Screenshot_2020-10-31_at_4.51.53_PM_wwfxri.png)
+
+![alt text](https://res.cloudinary.com/de8vrxbqq/image/upload/v1604160843/samples/Screenshot_2020-10-31_at_4.52.08_PM_ijr1d1.png)
+
+![alt text](https://res.cloudinary.com/de8vrxbqq/image/upload/v1604160884/samples/Screenshot_2020-10-31_at_4.52.25_PM_ydbxrc.png)
+
+![alt text](https://res.cloudinary.com/de8vrxbqq/image/upload/v1604160925/samples/Screenshot_2020-10-31_at_4.52.37_PM_ql5uq7.png)
+
+![alt text](https://res.cloudinary.com/de8vrxbqq/image/upload/v1604160952/samples/Screenshot_2020-10-31_at_4.52.49_PM_kzs4b0.png)
+
+
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+You can leave a message for me <nzubennamani@gmail.com> here and I will be happy to work with you
+
+## Developer
+Nzubechukwu Nnamani
